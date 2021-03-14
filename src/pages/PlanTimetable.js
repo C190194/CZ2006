efe6f361624f4f92cf97aa887fd6f6afbfaec0a1
Chiazhe <68742803+Chiazhe@users.noreply.github.com>
2021-03-14 16:00:18 +0800

    Zipeng Handsome

--- src/pages/PlanTimetable.js
+++ src/pages/PlanTimetable.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import {
+  Scheduler,
+  WeekView,
+  Appointments,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import { Container, Row, Col } from "reactstrap";
+
+import appointments from "../shares/today-appointments";
 
 function Planner() {
   return (
-    <div className='plan'>
-      <h1>Plan Timetable</h1>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Paper>
+            <Scheduler data={appointments} height={660}>
+              <WeekView startDayHour={9} endDayHour={19} />
+              <Appointments />
+            </Scheduler>
+          </Paper>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { Button, Form } from "reactstrap";
 import './Register.css'
 
 function Register() {
