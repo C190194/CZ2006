5a7435746cf07481169461835916dcd0fe250fa2
liew312 <56454775+liew312@users.noreply.github.com>
2021-03-30 18:27:18 +0800

    Update PlannerIndexComponent.js

--- src/components/PlannerIndexComponent.js
+++ src/components/PlannerIndexComponent.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import axios from 'axios';
+import axios from "axios";
 
 import "./ComponentsStyle.css";
 
@@ -104,12 +104,21 @@ export default function ShareTimetable(props) {
   const setCombinations = planTimetableContext.setCombinations;
   const currentTimeTablePage = planTimetableContext.currentTimeTablePage;
   const setIsPlanClicked = planTimetableContext.setIsPlanClicked;
+  // const allowClashCC = planTimetableContext.allowClashCC;
+  const setAllowClashCC = planTimetableContext.setAllowClashCC;
+
+  useEffect(() => {
+    console.log("index rerendered");
+  });
+
   //Backend: this method will retrieve all course indexes then call backend method to return timetables
   //if clash then give a error message
   //convert courseDivs to courses
   const planCourse = (temp_course_divs) => {
     // console.log(temp_course_divs);
     //pass course to backend
+    // setIsPlanClicked(true);
+
     const temp_course_arr = temp_course_divs.map((item) => {
       if (item.isIndexFixed) {
         let tempCourse = { ...item.course };
@@ -120,13 +129,14 @@ export default function ShareTimetable(props) {
       }
     });
 
-    axios.post('/user/login',{
-      email: 'astha@gmail.com',
-      password: 'astha123'
-    })
-  .then((response) => {
-    console.log(response.data);
-  });
+    axios
+      .post("/user/login", {
+        email: "astha@gmail.com",
+        password: "astha123",
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
     // console.log(temp_course_arr);
 
     //testing
@@ -228,6 +238,12 @@ export default function ShareTimetable(props) {
       delete tempCs[i][tempCourseDiv.course.courseCode];
     }
 
+    setAllowClashCC((prevstate) =>
+      prevstate.filter((item) => item !== tempCourseDiv.course.courseCode)
+    );
+    //delete the course code in allowclash array
+    // setall;
+
     setCombinations(tempCs);
   };
 
@@ -266,7 +282,6 @@ export default function ShareTimetable(props) {
       <Button
         className="btn-warning"
         onClick={() => {
-          setIsPlanClicked(true);
           planCourse(courseDivs);
         }}
       >
