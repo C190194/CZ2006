23f906de51f32e60ce31588a07dfc7881fa9c0aa
liew312 <56454775+liew312@users.noreply.github.com>
2021-04-01 02:01:55 +0800

    Update ShareTimetableComponent.js

--- src/components/ShareTimetableComponent.js
+++ src/components/ShareTimetableComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { DropdownButton, Dropdown } from "react-bootstrap";
 import "./ComponentsStyle.css";
 import {
@@ -10,91 +10,163 @@ import {
   ModalHeader,
   ModalBody,
 } from "reactstrap";
+import { FormatColorResetOutlined } from "@material-ui/icons";
 
-export default class ShareTimetableComponent extends Component {
-  constructor(props) {
-    super(props);
+export default function ShareTimetableComponent() {
+  const [state, setState] = useState({ isNavOpen: false, isModalOpen: false });
 
-    this.toggleNav = this.toggleNav.bind(this);
-    this.toggleModal = this.toggleModal.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-
-    this.state = {
-      isNavOpen: false,
-      isModalOpen: false,
-    };
-  }
-
-  toggleNav() {
-    this.setState({
-      isNavOpen: !this.state.isNavOpen,
+  function toggleNav() {
+    setState({
+      ...state,
+      isNavOpen: !state.isNavOpen,
     });
   }
 
-  toggleModal() {
-    this.setState({
-      isModalOpen: !this.state.isModalOpen,
+  function toggleModal() {
+    setState({
+      ...state,
+      isModalOpen: !state.isModalOpen,
     });
   }
 
-  handleLogin(event) {
-    this.toggleModal();
-    alert(
-      "Username: " +
-        this.username.value +
-        " Password: " +
-        this.password.value +
-        " Remember: " +
-        this.remember.checked
-    );
-    event.preventDefault();
-  }
+  // function handleLogin(event) {
+  //   toggleModal();
+  //   alert(
+  //     "Username: " +
+  //       username.value +
+  //       " Password: " +
+  //       password.value +
+  //       " Remember: " +
+  //       remember.checked
+  //   );
+  //   event.preventDefault();
+  // }
 
-  subShareTimetableComponent() {
+  function subShareTimetableComponent() {
     return (
       <div>
         <div className="share-timetable">
           <p>Choose a timetable to share</p>
           <DropdownButton id="dropdown-basic-button" title="Select a timetable">
-            <Dropdown.Item class="dropdown" href="#/action-1">
-              Timetable 1
-            </Dropdown.Item>
-            <Dropdown.Item class="dropdown" href="#/action-2">
-              Timetable 2
-            </Dropdown.Item>
-            <Dropdown.Item class="dropdown" href="#/action-3">
-              Timetable 3
-            </Dropdown.Item>
+            <Dropdown.Item class="dropdown">Timetable 1</Dropdown.Item>
+            <Dropdown.Item class="dropdown">Timetable 2</Dropdown.Item>
+            <Dropdown.Item class="dropdown">Timetable 3</Dropdown.Item>
           </DropdownButton>
         </div>
       </div>
     );
   }
 
-  render() {
-    return (
-      <>
-        <Row>
-          <div className="col-9" />
-          <Button className="col-3" outline onClick={this.toggleModal}>
-            <span className="fa fa-sign-in fa-lg"></span> Share Timetable
-          </Button>
-        </Row>
+  return (
+    <>
+      <Row>
+        <div className="col-9" />
+        <Button className="col-3" outline onClick={toggleModal}>
+          <span className="fa fa-sign-in fa-lg"></span> Share Timetable
+        </Button>
+      </Row>
 
-        <Modal
-          size="lg"
-          isOpen={this.state.isModalOpen}
-          toggle={this.toggleModal}
-          fade={false}
-        >
-          <ModalHeader toggle={this.toggleModal}>
-            <h2>Share Timetable</h2>
-          </ModalHeader>
-          <ModalBody class="modal fade bd-example-modal-lg">
-            {this.subShareTimetableComponent()}
-          </ModalBody>
-        </Modal>
-      </>
-    );
-  }
+      <Modal
+        size="lg"
+        isOpen={state.isModalOpen}
+        toggle={toggleModal}
+        fade={false}
+      >
+        <ModalHeader toggle={toggleModal}>
+          <h2>Share Timetable</h2>
+        </ModalHeader>
+        <ModalBody class="modal fade bd-example-modal-lg">
+          {subShareTimetableComponent()}
+        </ModalBody>
+      </Modal>
+    </>
+  );
 }
+
+// export default class ShareTimetableComponent extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.toggleNav = this.toggleNav.bind(this);
+//     this.toggleModal = this.toggleModal.bind(this);
+//     this.handleLogin = this.handleLogin.bind(this);
+
+//     this.state = {
+//       isNavOpen: false,
+//       isModalOpen: false,
+//     };
+//   }
+
+//   toggleNav() {
+//     this.setState({
+//       isNavOpen: !this.state.isNavOpen,
+//     });
+//   }
+
+//   toggleModal() {
+//     this.setState({
+//       isModalOpen: !this.state.isModalOpen,
+//     });
+//   }
+
+//   handleLogin(event) {
+//     this.toggleModal();
+//     alert(
+//       "Username: " +
+//         this.username.value +
+//         " Password: " +
+//         this.password.value +
+//         " Remember: " +
+//         this.remember.checked
+//     );
+//     event.preventDefault();
+//   }
+
+//   subShareTimetableComponent() {
+//     return (
+//       <div>
+//         <div className="share-timetable">
+//           <p>Choose a timetable to share</p>
+//           <DropdownButton id="dropdown-basic-button" title="Select a timetable">
+//             <Dropdown.Item class="dropdown" href="#/action-1">
+//               Timetable 1
+//             </Dropdown.Item>
+//             <Dropdown.Item class="dropdown" href="#/action-2">
+//               Timetable 2
+//             </Dropdown.Item>
+//             <Dropdown.Item class="dropdown" href="#/action-3">
+//               Timetable 3
+//             </Dropdown.Item>
+//           </DropdownButton>
+//         </div>
+//       </div>
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <>
+//         <Row>
+//           <div className="col-9" />
+//           <Button className="col-3" outline onClick={this.toggleModal}>
+//             <span className="fa fa-sign-in fa-lg"></span> Share Timetable
+//           </Button>
+//         </Row>
+
+//         <Modal
+//           size="lg"
+//           isOpen={this.state.isModalOpen}
+//           toggle={this.toggleModal}
+//           fade={false}
+//         >
+//           <ModalHeader toggle={this.toggleModal}>
+//             <h2>Share Timetable</h2>
+//           </ModalHeader>
+//           <ModalBody class="modal fade bd-example-modal-lg">
+//             {this.subShareTimetableComponent()}
+//           </ModalBody>
+//         </Modal>
+//       </>
+//     );
+//   }
+// }
