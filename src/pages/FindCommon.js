49b0c13ae25277f5267c710b9ac7cddc9e4a77de
liew312 <56454775+liew312@users.noreply.github.com>
2021-04-08 19:58:08 +0800

    integration commits

--- src/pages/FindCommon.js
+++ src/pages/FindCommon.js
@@ -13,6 +13,7 @@ import Grid from "@material-ui/core/Grid";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import Paper from "@material-ui/core/Paper";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   toggleContainer: {
@@ -139,13 +140,83 @@ export default function FindCommon() {
     ];
     setSelectedICSfiles(tempSelectedICSfiles);
   };
+  const week1 = new Date("2021-01-11T00:00:00Z");
+  const week2 = new Date("2021-01-18T00:00:00Z");
+  const week3 = new Date("2021-01-25T00:00:00Z");
+  const week4 = new Date("2021-02-01T00:00:00Z");
+  const week5 = new Date("2021-02-08T00:00:00Z");
+  const week6 = new Date("2021-02-15T00:00:00Z");
+  const week7 = new Date("2021-02-22T00:00:00Z");
+  const recess = new Date("2021-03-01T00:00:00Z");
+  const week8 = new Date("2021-03-08T00:00:00Z");
+  const week9 = new Date("2021-03-15T00:00:00Z");
+  const week10 = new Date("2021-03-22T00:00:00Z");
+  const week11 = new Date("2021-03-29T00:00:00Z");
+  const week12 = new Date("2021-04-05T00:00:00Z");
+  const week13 = new Date("2021-04-12T00:00:00Z");
+  const end = new Date("2021-04-19T00:00:00Z");
 
+  const teaching_weeks = [
+    week1,
+    week2,
+    week3,
+    week4,
+    week5,
+    week6,
+    week7,
+    week8,
+    week9,
+    week10,
+    week11,
+    week12,
+    week13,
+    end,
+  ];
+
+  function getWeek() {
+    let now = new Date();
+    if (recess < now && now < week8) {
+      return -1; // Recess week
+    } else if (end < now) {
+      return 14; // Teaching weeks have ended
+    }
+    for (let i = 0; i < 13; i++) {
+      if (now < teaching_weeks[i]) {
+        return i;
+        // i = 0 means before week1
+      }
+    }
+  }
   //call backend method
   const submitFiles = () => {
     const reqbody = { icsList: [] };
     reqbody.icsList = selectedICSfiles.map((item) => item.fileData);
+    reqbody.week = getWeek();
     // reqbody.week = getCurrentWeek();
     console.log(reqbody);
+
+    let axiosConfig = {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+
+    axios
+      .post("/appointment/get_appointments", reqbody, axiosConfig)
+      .then((response) => {
+        console.log(response);
+        console.log(response.data);
+        const tempSelectedICSfiles = [...selectedICSfiles];
+        response.data[0].forEach((element, idx) => {
+          // console.log(idx);
+          tempSelectedICSfiles[idx].results[0] = element;
+        });
+        response.data[1].forEach((element, idx) => {
+          //  console.log(idx);
+          tempSelectedICSfiles[idx].results[1] = element;
+        });
+      });
   };
 
   //call backend method
@@ -236,3 +307,11 @@ export default function FindCommon() {
     </div>
   );
 }
+//         <Link
+//           to={{
+//             pathname: "/savedtimetables",
+//             state: "Select a timetable for find common",
+//           }}
+//         >
+//           <Button>go to savedtt</Button>
+//         </Link>
--- src/pages/randomtest.js
+++ src/pages/randomtest.js
@@ -27,20 +27,52 @@ if (something) {
 } else {
   console.log("no");
 }
-const week1 = new Date("2021-01-11T00:00:00");
+const week1 = new Date("2021-01-11T00:00:00Z");
+const week2 = new Date("2021-01-18T00:00:00Z");
+const week3 = new Date("2021-01-25T00:00:00Z");
+const week4 = new Date("2021-02-01T00:00:00Z");
+const week5 = new Date("2021-02-08T00:00:00Z");
+const week6 = new Date("2021-02-15T00:00:00Z");
+const week7 = new Date("2021-02-22T00:00:00Z");
+const recess = new Date("2021-03-01T00:00:00Z");
+const week8 = new Date("2021-03-08T00:00:00Z");
+const week9 = new Date("2021-03-15T00:00:00Z");
+const week10 = new Date("2021-03-22T00:00:00Z");
+const week11 = new Date("2021-03-29T00:00:00Z");
+const week12 = new Date("2021-04-05T00:00:00Z");
+const week13 = new Date("2021-04-12T00:00:00Z");
+const end = new Date("2021-04-19T00:00:00Z");
 
-function getCurrentWeek() {
-  const dateNow = new Date.now();
-  var diff = getDaysDiff(dateNow, week1);
-  var weeks = diff / 7;
-  if (weeks > 7) {
-    weeks--;
+const teaching_weeks = [
+  week1,
+  week2,
+  week3,
+  week4,
+  week5,
+  week6,
+  week7,
+  week8,
+  week9,
+  week10,
+  week11,
+  week12,
+  week13,
+  end,
+];
+
+function getWeek() {
+  let now = new Date();
+  if (recess < now && now < week8) {
+    return -1; // Recess week
+  } else if (end < now) {
+    return 14; // Teaching weeks have ended
   }
-  return weeks;
+  for (let i = 0; i < 13; i++) {
+    if (now < teaching_weeks[i]) {
+      return i;
+      // i = 0 means before week1
+    }
+  }
 }
 
-function getDaysDiff(date1, date2) {
-  const diffTime = Math.abs(date2 - date1);
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  return diffDays;
-}
+console.log(getWeek());
