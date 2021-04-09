04f374300e3740d2098d031aa47716779c3b3a73
liew312 <56454775+liew312@users.noreply.github.com>
2021-04-10 01:13:03 +0800

    Update PlannerCalendarComponent.js

--- src/components/PlannerCalendarComponent.js
+++ src/components/PlannerCalendarComponent.js
@@ -7,7 +7,10 @@ import {
   AppointmentTooltip,
   Resources,
 } from "@devexpress/dx-react-scheduler-material-ui";
-
+import Grid from "@material-ui/core/Grid";
+import GroupIcon from "@material-ui/icons/Group";
+import RoomIcon from "@material-ui/icons/Room";
+import ScheduleIcon from "@material-ui/icons/Schedule";
 import { ViewState } from "@devexpress/dx-react-scheduler";
 import { resourcesData } from "./resources";
 import "./calendar.css";
@@ -72,6 +75,50 @@ export default function PlannerCalendarComponent(props) {
     );
   };
 
+  const AppoinmentTooltipContent = ({
+    children,
+    appointmentData,
+    classes,
+    ...restProps
+  }) => {
+    return (
+      <AppointmentTooltip.Content
+        {...restProps}
+        appointmentData={appointmentData}
+      >
+        <Grid container alignItems="center">
+          <Grid item xs={2} style={{ textAlign: "center" }}>
+            <GroupIcon />
+          </Grid>
+          <Grid item xs={10}>
+            <span>{appointmentData.group}</span>
+          </Grid>
+          <Grid item xs={2} style={{ textAlign: "center" }}>
+            <RoomIcon />
+          </Grid>
+          <Grid item xs={10}>
+            <span>{appointmentData.location}</span>
+          </Grid>
+        </Grid>
+
+        {appointmentData.remarks !== "" ? (
+          <div>
+            <Grid container alignItems="center">
+              <Grid item xs={2} style={{ textAlign: "center" }}>
+                <ScheduleIcon />
+              </Grid>
+              <Grid item xs={10}>
+                <span>{appointmentData.remarks}</span>
+              </Grid>
+            </Grid>
+          </div>
+        ) : (
+          ""
+        )}
+      </AppointmentTooltip.Content>
+    );
+  };
+
   // const TimeScaleLayout = () => {
   //   return <WeekView.TimeScaleLayout height={20} />;
   // };
@@ -93,7 +140,10 @@ export default function PlannerCalendarComponent(props) {
             // timeScaleLayoutComponent={TimeScaleLayout}
           />
           <Appointments appointmentContentComponent={AppointmentContent} />
-          <AppointmentTooltip showCloseButton />
+          <AppointmentTooltip
+            showCloseButton
+            contentComponent={AppoinmentTooltipContent}
+          />
           <Resources data={resources} mainResourceName="courseDivID" />
         </Scheduler>
       </div>
